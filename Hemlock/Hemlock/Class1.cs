9a38cf5bb5e28701527a5add9b7354f6da996f0b
mattdurham <mattdurham@ppog.org>
2015-07-11 11:42:11 -0400

    Initial Check In

---
+++ Hemlock/Hemlock/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemlock
+{
+    public class Class1
+    {
+    }
+}
